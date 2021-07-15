3b0e6dc2afab1340a9f322693c4546b8784b2e09
PreetBhadana <preetbhadana5@gmail.com>
2021-07-15 17:58:24 +0530

    Done Date methods from Date and time Topic of Ruby

---
+++ Ruby/Ruby_Practice/Date_and_time_components.rb
@@ -0,0 +1,18 @@
+#Date and time components --
+
+time1 = Time.new
+
+#Components --
+
+puts "Current Time : " + time1.inspect
+
+puts time1.year # To show year from date
+puts time1.month # To show month from date
+puts time1.day # To show day of the date
+puts time1.wday # TO show day according week like sunday monday etc in integer
+puts time1.yday # To show day according in year
+puts time1.hour # To show hour of time
+puts time1.min  # To show minute of time
+puts time1.sec  # To show seconds of time
+puts time1.usec # To show nanoseconds of time
+puts time1.zone # To show zone of time.
