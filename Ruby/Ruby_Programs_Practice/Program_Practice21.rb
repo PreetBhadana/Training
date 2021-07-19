d1ab21be7c25d4ec1050b587d6b3c7a89c8e5bb8
PreetBhadana <preetbhadana5@gmail.com>
2021-07-19 12:20:02 +0530

    Iterate and count hash value from array practice Program

---
+++ Ruby/Ruby_Programs_Practice/Program_Practice21.rb
@@ -0,0 +1,81 @@
+'''
+Medium 4
+
+Given an array of scrabble tiles, create a function that outputs the maximum possible score a player can achieve by summing up the total number of points for all the tiles in their hand. Each hand contains 7 scrabble tiles.
+
+Here\'s an example hand:
+
+[
+  { "tile" => "N", "score" => 1 },
+  { "tile" => "K", "score" => 5 },
+  { "tile" => "Z", "score" => 10 },
+  { "tile" => "X", "score" => 8 },
+  { "tile" => "D", "score" => 2 },
+  { "tile" => "A", "score" => 1 },
+  { "tile" => "E", "score" => 1 }
+]
+The players maximum score from playing all these tiles would be 1 + 5 + 10 + 8 + 2 + 1 + 1, or 28.
+
+Examples
+maximum_score([
+  { "tile" => "N", "score" => 1 },
+  { "tile" => "K", "score" => 5 },
+  { "tile" => "Z", "score" => 10 },
+  { "tile" => "X", "score" => 8 },
+  { "tile" => "D", "score" => 2 },
+  { "tile" => "A", "score" => 1 },
+  { "tile" => "E", "score" => 1 }
+]) ➞ 28
+
+maximum_score([
+  { "tile" => "B", "score" => 2 },
+  { "tile" => "V", "score" => 4 },
+  { "tile" => "F", "score" => 4 },
+  { "tile" => "U", "score" => 1 },
+  { "tile" => "D", "score" => 2 },
+  { "tile" => "O", "score" => 1 },
+  { "tile" => "U", "score" => 1 }
+]) ➞ 15
+Notes
+Here, each tile is represented as a hash with two keys: tile and score.
+'''
+
+
+def maximum_score(arr)
+    #puts arr
+    sum = 0
+    for i in arr
+        #print i
+        for j,k in i
+            #print j[:score]
+            #print "\n"
+            if j == "score"
+                sum  = sum + k
+                #print k.class
+                #print "\n"
+            end
+        end
+    end
+    return sum 
+
+end
+
+puts maximum_score([
+    { "tile" => "B", "score" => 2 },
+    { "tile" => "V", "score" => 4 },
+    { "tile" => "F", "score" => 4 },
+    { "tile" => "U", "score" => 1 },
+    { "tile" => "D", "score" => 2 },
+    { "tile" => "O", "score" => 1 },
+    { "tile" => "U", "score" => 1 }
+  ])
+
+puts maximum_score([
+    { "tile" => "N", "score" => 1 },
+    { "tile" => "K", "score" => 5 },
+    { "tile" => "Z", "score" => 10 },
+    { "tile" => "X", "score" => 8 },
+    { "tile" => "D", "score" => 2 },
+    { "tile" => "A", "score" => 1 },
+    { "tile" => "E", "score" => 1 }
+  ])
