1e4121469f7d0b6c904713a4f9262b9a839cbb81
PreetBhadana <preetbhadana5@gmail.com>
2021-07-21 16:04:29 +0530

    Functions in js practice program

---
+++ Java_Script/Function_in_js.js
@@ -0,0 +1,22 @@
+//Function_in_js
+
+//Simple function ---
+
+function greet(){
+    console.log('Hello World')
+}
+greet();
+
+// Parametrized functions ---
+function greet(name){
+    console.log('Hello '+name)
+}
+greet('John')
+
+// Multiple parameters functions ---
+
+function greet(name, lastName)
+{
+    console.log('Hello '+name+' '+lastName)
+}
+greet('John', 'Smith')
