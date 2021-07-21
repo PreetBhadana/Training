1ada81e8a1867f38a61450f271dc6d03c46d0fed
PreetBhadana <preetbhadana5@gmail.com>
2021-07-21 16:03:08 +0530

    Variables and datatype in js practice program

---
+++ Java_Script/Variables_in_js.js
@@ -0,0 +1,22 @@
+// Variable in java script
+
+let name = "Preet"
+let age = 25
+let isApproved = true
+let fisrtName = undefined
+let selectedColor = null
+
+console.log(name)
+console.log(typeof name)
+
+console.log(age)
+console.log(typeof age)
+
+console.log(isApproved)
+console.log(typeof isApproved)
+
+console.log(fisrtName)
+console.log(typeof fisrtName)
+
+console.log(selectedColor)
+console.log(typeof selectedColor)
