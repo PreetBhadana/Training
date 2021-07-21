267e232ee40d1652b12d75479ed7659b2ad783fb
PreetBhadana <preetbhadana5@gmail.com>
2021-07-21 16:03:39 +0530

    Objects in js practice program

---
+++ Java_Script/Objects_in_js.js
@@ -0,0 +1,16 @@
+//Objects in js
+let person = {
+    name: "Preet",
+    age: 25
+};
+
+console.log(person)
+
+person.name = 'Nitin'
+
+console.log(person)
+console.log(person.name)
+
+person['name'] = 'Neha'
+
+console.log(person['name'])
